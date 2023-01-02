aa128932dae42bb0b53b36abf27b6a870e101c8d
semanurakkuzu <semanurakkuzu@gmail.com>
2023-01-02 11:59:32 +0300

    added variants index

--- src/components/MyProduct.vue
+++ src/components/MyProduct.vue
@@ -11,13 +11,22 @@
         <ul>
           <li v-for="(item, index) in details" :key="index">{{ item }}</li>
         </ul>
-        <div v-for="variant in variants" :key="variant.variantId" class="box-size"
-        :style="{backgroundColor:variant.variantColor}"
-        @mouseover="updateProduct(variant.variantImage)">
-        </div>
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.variantId"
+          class="box-size"
+          :style="{ backgroundColor: variant.variantColor }"
+          @mouseover="updateProduct(index)"
+        ></div>
         <div class="row">
           <div class="col-auto">
-            <button class="btn btn-primary" @click="cart=cart+1" :disabled="!inStock">Add to Cart</button>
+            <button
+              class="btn btn-primary"
+              @click="cart = cart + 1"
+              :disabled="!inStock"
+            >
+              Add to Cart
+            </button>
           </div>
           <div class="col">
             <p>Cart:{{ cart }}</p>
@@ -29,51 +38,57 @@
 </template>
 
 <script>
-
-
 export default {
   name: "MyProduct",
   data() {
     return {
       brand: "Vue Mastery",
       product: "socks",
-      image:
-        "https://cdn.shopify.com/s/files/1/1409/0762/products/Red_Cloud_SOck_copy_800x.jpg?v=1645761997",
-      inStock: true,
+      selectVariants: 0,
       details: ["80% cotton", "20% polyester", "Gender-Neutral"],
       variants: [
         {
           variantId: 1,
           variantColor: "red",
-          variantImage:"https://cdn.shopify.com/s/files/1/1409/0762/products/Red_Cloud_SOck_copy_800x.jpg?v=1645761997"
+          variantImage:
+            "https://cdn.shopify.com/s/files/1/1409/0762/products/Red_Cloud_SOck_copy_800x.jpg?v=1645761997",
+          variantQuantity: 10,
         },
         {
           variantId: 2,
           variantColor: "purple",
-          variantImage:"https://cdn.shopify.com/s/files/1/1409/0762/products/CloudSockinEggplant_800x.png?v=1645761997"
+          variantImage:
+            "https://cdn.shopify.com/s/files/1/1409/0762/products/CloudSockinEggplant_800x.png?v=1645761997",
+          variantQuantity: 0,
         },
       ],
       cart: 0,
+    };
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectVariants].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectVariants].variantQuantity;
     }
   },
-  computed:{
-    title(){
-      return this.brand +' '+ this.product
-    }
+
+  methods: {
+    updateProduct(index) {
+      this.selectVariants = index;
+    },
   },
-   methods:{
-    updateProduct(variantImage){
-      this.image=variantImage
-    }
-   },
-  
 };
 </script>
 
 <style>
 .box-size {
-    width:40px; 
-    height:40px;
-    margin: 5px;
+  width: 40px;
+  height: 40px;
+  margin: 5px;
 }
 </style>
